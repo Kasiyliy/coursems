579c4392fc930936bb0f5bed0d5e2fcb3f23517d
air <asilkhan-al@mail.ru>
2019-12-27 23:57:48 +0600

    Datatables added

--- resources/views/admin/courses/index.blade.php
+++ resources/views/admin/courses/index.blade.php
@@ -4,13 +4,13 @@
     <div class="container-fluid">
         <div class="row justify-content-center">
             <div class="col-sm-12">
-                <div class="panel"  style="padding: 10px;">
+                <div class="panel" style="padding: 10px;">
                     <div class="panel-header">
                         <h2>Курсы</h2>
                         <a class="btn btn-success btn-sm" href="{{route('course.create')}}">Добавить</a>
                     </div>
                     <div class="panel-body">
-                        <table class="table table-hover table-responsive">
+                        <table class="table table-hover table-responsive" id="dataTable">
                             <thead>
                             <tr>
                                 <th>ID</th>
@@ -26,18 +26,23 @@
                                     <td>{{$course->name}}</td>
                                     <td>{{$course->visible ? 'Да' : 'Нет'}}</td>
                                     <td class="d-flex">
-                                        <button type="button" class="btn btn-danger btn-xs mr-1" data-toggle="modal" data-target="#exampleModal{{$course->id}}">
+                                        <button type="button" class="btn btn-danger btn-xs mr-1" data-toggle="modal"
+                                                data-target="#exampleModal{{$course->id}}">
                                             <span class="fa fa-trash"></span> Удалить
                                         </button>
 
                                         <!-- Modal -->
-                                        <div class="modal fade" id="exampleModal{{$course->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                        <div class="modal fade" id="exampleModal{{$course->id}}" tabindex="-1"
+                                             role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                                             <div class="modal-dialog" role="document">
                                                 <div class="modal-content">
-                                                    <form method="post" action="{{route('course.delete', ['id' => $course->id ])}}">
+                                                    <form method="post"
+                                                          action="{{route('course.delete', ['id' => $course->id ])}}">
                                                         <div class="modal-header">
-                                                            <h5 class="modal-title" id="exampleModalLabel">Предупреждение!</h5>
-                                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                            <h5 class="modal-title" id="exampleModalLabel">
+                                                                Предупреждение!</h5>
+                                                            <button type="button" class="close" data-dismiss="modal"
+                                                                    aria-label="Close">
                                                                 <span aria-hidden="true">&times;</span>
                                                             </button>
                                                         </div>
@@ -48,19 +53,27 @@
 
                                                         </div>
                                                         <div class="modal-footer">
-                                                            <button type="button" class="btn btn-secondary btn-sm" data-dismiss="modal">Отмена</button>
-                                                            <input type="submit" value="Удалить" class="btn btn-danger btn-sm mr-1">
+                                                            <button type="button" class="btn btn-secondary btn-sm"
+                                                                    data-dismiss="modal">Отмена
+                                                            </button>
+                                                            <input type="submit" value="Удалить"
+                                                                   class="btn btn-danger btn-sm mr-1">
                                                         </div>
                                                     </form>
                                                 </div>
                                             </div>
                                         </div>
 
-                                        <a href="{{route('course.edit' ,['id'=>$course->id ])}}" class="btn-xs btn btn-primary"><span class="fa fa-edit"></span> Изменить</a>
-                                        <a href="{{route('course.details' ,['id'=>$course->id ])}}" class="btn-xs btn btn-info"><span class="fa fa-book"></span> Уроки</a>
-                                        <form action="{{route('course.toggle.visible' ,['id'=>$course->id ])}}" method="post">
+                                        <a href="{{route('course.edit' ,['id'=>$course->id ])}}"
+                                           class="btn-xs btn btn-primary"><span class="fa fa-edit"></span> Изменить</a>
+                                        <a href="{{route('course.details' ,['id'=>$course->id ])}}"
+                                           class="btn-xs btn btn-info"><span class="fa fa-book"></span> Уроки</a>
+                                        <form action="{{route('course.toggle.visible' ,['id'=>$course->id ])}}"
+                                              method="post">
                                             {{csrf_field()}}
-                                            <button type="submit" class="btn-xs btn btn-warning"><span class="fa fa-eye"></span> {{$course->visible ? 'Скрыть' : 'Опубликовать'}}</button>
+                                            <button type="submit" class="btn-xs btn btn-warning"><span
+                                                        class="fa fa-eye"></span> {{$course->visible ? 'Скрыть' : 'Опубликовать'}}
+                                            </button>
                                         </form>
                                     </td>
                                 </tr>
@@ -72,4 +85,11 @@
             </div>
         </div>
     </div>
+@endsection
+
+
+@section('datatable')
+
+    @include('layouts.datatable')
+
 @endsection
